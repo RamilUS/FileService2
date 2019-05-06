b3038a37e68e0e885257cb2d6126667c1ea0a4f9
RamilUS <ramil90us@gmail.com>
2019-05-06 21:42:14 +0300

    Revert "Спроектирована ветка загрузки файлов в БД, скачивание файлов из БД и удаление файлов пользователя. Доработаны тесты Добавлены контроллеры FilePagesController и FileRestController. Слой Dao заменен Repository extends JpaRepository. Убраны  wildcard import (*). Таблицы созданы sql скриптом. Введены ограничения по количеству символов в строках таблиц. Убраны неиспользуемые поля из таблиц. Добавлены тесты. Добавлено логгирование. Доработаны контроллеры по получению сервисных сообщение при не выпонении условий загрузки файлов Доработан сервис загрузки файла с выводом сервисных сообщений. Вся бизнес-логика убрана из контроллеров в сервис. Убраны сущность и таблица "Пользователь" . Доработаны логи и тесты Исправлены контроллеры"
    This reverts commit 92dd951d

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS file(
-    id SERIAL       NOT NULL PRIMARY KEY ,
-    version         INTEGER ,
-    file_name       VARCHAR(100) NOT NULL,
-    file_data BYTEA NOT NULL,
-    description     VARCHAR(150) NOT NULL,
-    download_count  INTEGER
-);
